349d4c05c83af710e678e8a7c5e772184491216b
Albrim Paqarizi <albrimpa@gmail.com>
2020-10-13 15:33:04 +0200

    Update solutions page

---
+++ src/pages/Solutions.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./main.css";
+import SolutionsComponent from "../components/Solutions/index";
+
+const Solutions = () => {
+  return (
+      <SolutionsComponent />
+  );
+};
+
+export default Solutions;
--- src/route.js
+++ src/route.js
@@ -4,12 +4,11 @@ import Layout from "./layout/DefaultLayout";
 import ScrollToTop from "./components/Shared/ScrollToTop";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Service from "./pages/Service";
+import Solutions from "./pages/Solutions";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import SignUp from "./pages/SignUp";
-import Pages from "./pages/Pages";
 import OurTeam from "./pages/OurTeam";
 
 function route() {
@@ -20,11 +19,10 @@ function route() {
         <Switch>
           <Route path="/" exact render={() => <Home />} />
           <Route path="/about" exact render={() => <About />} />
-          <Route path="/service" exact render={() => <Service />} />
+          <Route path="/solutions" exact render={() => <Solutions />} />
           <Route path="/contact" exact render={() => <Contact />} />
           <Route path="/login" exact render={() => <Login />} />
           <Route path="/signup" exact render={() => <SignUp />} />
-          <Route path="/pages" exact render={() => <Pages />} />
           <Route path="/our-team" exact render={() => <OurTeam />} />
           <Route to="/404" render={() => <PageNotFound />} />
           <Redirect to="/404" />
